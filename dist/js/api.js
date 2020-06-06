733278390c6e7e6ad98ea75e92b93a31c4ee2f94
Emrikcah <curryandr3@gmail.com>
2020-06-06 14:03:18 -0400

    made alot of changes

--- dist/js/api.js
+++ dist/js/api.js
@@ -1,17 +1,15 @@
-const results = document.querySelector(".results");
 const randPeople = fetch("https://randomuser.me/api/?results=12");
+const mainCard = document.querySelector(".main-card");
 
-
-
+// Get the data from the API
 randPeople
   .then((response) => response.json())
   .then((data) => getInformation(data.results));
 
+/**loop through each element and  create the HTML markup and store it in the html variable */
 function getInformation(data) {
-  
-  let html = "";
   data.forEach((element) => {
-    html += `
+    mainCard.innerHTML += `
     <div class="card">
              <img src='${element.picture.medium}' class='img' alt>
         <div class="group">
@@ -23,5 +21,18 @@ function getInformation(data) {
        
         `;
   });
-  results.innerHTML = html;
+/**NOTE: cards cant be in the global space because it would run before the dynamic html was created and show up as an empty nodeList */
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => card.addEventListener("click", modalDisplay));
 }
+
+function modalDisplay() {
+  console.log("working");
+
+    const lightbox = document.createElement("div");
+    lightbox.id = "lightbox";
+    document.body.appendChild(lightbox);
+    lightbox.classList.add("active");
+}
+
+// Event Handlers
