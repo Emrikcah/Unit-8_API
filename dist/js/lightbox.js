43b70517429a83c012dbbf3bafacff1ed63d44de
Emrikcah <curryandr3@gmail.com>
2020-06-06 14:03:55 -0400

    removed lightbox.js

--- dist/js/lightbox.js
+++
@@ -1,9 +0,0 @@
-const cards = document.querySelectorAll(".card");
-
-function imageClick() {
-  console.log('working');
-}
-
-cards.forEach((currentItem) => {
-  currentItem.addEventListener("click", imageClick);
-});
